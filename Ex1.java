78bf7c6528a93e09071ec1ba695b5a5b8235ef13
Rohan <rmandavkar98@gmail.com>
2022-01-05 17:54:25 +0530

    Initial

---
+++ Ex1.java
@@ -0,0 +1,37 @@
+package javaExamples;
+
+public class Ex1 {
+	
+	public void A()
+	{
+	System.out.println("A task");	
+	}
+	public void B()
+	{
+		System.out.println("B task");	
+
+	}
+	public void C()
+	{
+		System.out.println("C task");	
+
+	}
+		//java case sensitive
+	public static void Main(String[] args) {
+		// TODO Auto-generated method stub
+		Ex1 obj1=new Ex1();
+		
+		obj1.A();
+		obj1.B();
+		obj1.C();
+		System.out.println("hello");
+		System.out.println(0+9);
+	}
+	public static void main(String [] a)
+	{
+		
+		
+		
+	}
+
+}
